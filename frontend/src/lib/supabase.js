fdfb783ead625fd28e0aa1a514539308ab1616b6
Aastha Joshi <157534481+JoshiAastha04@users.noreply.github.com>
2026-03-15 11:54:53 -0700

    fix: supabase

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -3,9 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl  = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey  = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-console.log("URL:", supabaseUrl);
-console.log("KEY:", supabaseKey);
-
 if (!supabaseUrl || !supabaseKey) {
     throw new Error("Missing VITE_SUPABASE_URL or VITE_SUPABASE_ANON_KEY in .env");
 }
